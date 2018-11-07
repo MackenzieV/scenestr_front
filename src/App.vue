b4f76644b45ccb526723e14b08f80f3486aa30bc
Mackenzie V <lonesomegeorge87@gmail.com>
2018-11-07 16:21:27 -0600

    updated proposal

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="http://scenestr.com.au/images/scenestr-logos/Hi-res/scenestrlogo-black-cmyk-300dpi.jpg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img {
+  max-width: 150px;
+  max-height: 150px;
+}
+</style>
